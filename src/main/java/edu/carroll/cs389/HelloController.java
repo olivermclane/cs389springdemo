aa1552980e082de0a9be9af77debe5ca64dc040e
oliver mclane <o@mclane.cc>
2023-01-20 15:01:50 -0700

    changes to hellocontroller for thymeleaf template and added template for thymeleaf

--- src/main/java/edu/carroll/cs389/HelloController.java
+++ src/main/java/edu/carroll/cs389/HelloController.java
@@ -1,12 +1,12 @@
 package edu.carroll.cs389;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/hello")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "index";
     }
 }
